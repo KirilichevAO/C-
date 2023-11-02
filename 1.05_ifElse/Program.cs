86f7092d21565371314683aa128f56e4862c7cb6
North <North@MacBook-Air-MavBook.local>
2023-11-02 12:08:04 +0700

    1.05

---
+++ 1.05_ifElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if(userName == "Маша")
+{
+    Console.Write("Ура, это же ");
+    Console.Write(userName);
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(userName);
+}
