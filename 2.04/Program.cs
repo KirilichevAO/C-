b4a909dc8542f0385a98526a94f3c626a2556c7b
North <North@MacBook-Air-MavBook.local>
2023-11-08 17:37:32 +0700

    2.04

---
+++ 2.04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
+
+Console.Write("Введите первое число: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numThre = Convert.ToInt32(Console.ReadLine());
+int numMax = numOne;
+
+if(numMax < numTwo) numMax = numTwo;
+if(numMax < numThre) numMax = numThre;
+Console.Write($"{numOne}, {numTwo}, {numThre} -> {numMax}");
