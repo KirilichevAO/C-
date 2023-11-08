a923b7b06e00c75216e700f5d1709b0423327255
North <North@MacBook-Air-MavBook.local>
2023-11-08 10:16:39 +0700

    2.01

---
+++ 2.01/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+Console.Write("Введите число a: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int numTow = Convert.ToInt32(Console.ReadLine());
+
+
+if (numTow * numTow == numOne)
+{
+    Console.Write($"a = {numOne}, b = {numTow} -> да");
+}
+else
+{
+    Console.Write($"a = {numOne}, b = {numTow} ->Нет");
+}
