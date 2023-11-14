89875e0545d1fbb37fd8b529b20d1b5922963015
North <North@MacBook-Air-MavBook.local>
2023-11-14 13:40:42 +0700

    2.11

---
+++ 2.11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int numRandom = new Random().Next(100, 1000);
+
+int delNum(int num)
+{
+    int numOne = numRandom / 100;
+    int numTwo = numRandom % 10;
+    return numOne * 10 + numTwo;
+}
+
+int result = delNum(numRandom);
+Console.WriteLine($"{numRandom} -> {result}");
