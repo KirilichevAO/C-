a6e77c5bafefb4d60505dab61291f287f4f3fc07
North <North@MacBook-Air-MavBook.local>
2023-11-14 13:40:56 +0700

    2.12

---
+++ 2.12/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число
+// кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите первое число: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+
+// if(numOne % numTwo == 0) // перенесли это ветвление в метод Krat
+// {
+//     Console.Write($"{numOne}, {numTwo} -> кратно");
+// }
+// else
+// {
+//     Console.Write($"{numOne}, {numTwo} -> не кратно {numOne % numTwo}");
+// }
+
+int Krat(int num1, int num2)
+{
+    return num1 % num2 == 0 ? -1 : num1 % num2;
+}
+
+int result = Krat(numOne, numTwo);
+string res = result == -1 ? "кратно" : $"не кратно, остаток {result.ToString()}";
+Console.Write($"{numOne}, {numTwo} -> {res}");
+
