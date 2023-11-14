429928386b18f9d672ca61ff848e29f0e541704f
North <North@MacBook-Air-MavBook.local>
2023-11-14 15:27:06 +0700

    2.14

---
+++ 2.14/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Krat(int number)
+{
+    return number % 7 == 0 && number % 23 == 0 ? 1 : 0;
+}
+
+int result = Krat(num);
+string res = result == 1 ? "yes" : "no";
+Console.Write($"{num} -> {res}");
