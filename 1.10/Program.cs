7abd81ae15e7a3f77e301b57f3787835cb8fb5a5
North <North@MacBook-Air-MavBook.local>
2023-11-14 13:39:49 +0700

    1.10

---
+++ 1.10/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1, 12, 31, 41, 15, 16, 71, 18};
+int n = array.Length;
+int find = 41;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.Write(index);
+    }
+    index++;
+}
