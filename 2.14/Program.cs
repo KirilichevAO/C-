eeffc514525a8e5729bc4259f64e687672b4eb77
North <North@MacBook-Air-MavBook.local>
2023-11-14 15:46:31 +0700

    2.14v2

--- 2.14/Program.cs
+++ 2.14/Program.cs
@@ -3,14 +3,25 @@
 // 46 -> нет
 // 161 -> да
 
-Console.Write("Введите число: ");
+// Console.Write("Введите число: "); // один вариант решения
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int Krat(int number)
+// {
+//     return number % 7 == 0 && number % 23 == 0 ? 1 : 0;
+// }
+
+// int result = Krat(num);
+// string res = result == 1 ? "yes" : "no";
+// Console.Write($"{num} -> {res}");
+
+Console.Write("Введите число: "); // другой вариант решения
 int num = Convert.ToInt32(Console.ReadLine());
 
-int Krat(int number)
+bool Krat(int number, int a, int b)
 {
-    return number % 7 == 0 && number % 23 == 0 ? 1 : 0;
+    return number % a == 0 && number % b == 0;
 }
 
-int result = Krat(num);
-string res = result == 1 ? "yes" : "no";
-Console.Write($"{num} -> {res}");
+if (Krat(num, 7, 23)) Console.Write($"{num} -> yes");
+else Console.Write($"{num} -> no");
