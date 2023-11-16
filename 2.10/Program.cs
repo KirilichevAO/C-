d0be3801144a55b45c246b3158150b4692c92822
North <North@MacBook-Air-MavBook.local>
2023-11-16 12:34:56 +0700

    2.10

---
+++ 2.10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int TowNum(int number)
+{
+    return number % 100 / 10;
+}
+
+if (num.ToString().Length != 3) Console.Write($"Число {num} не трехзначное, попробуйте еще раз!!!");
+else Console.Write($"{num} -> {TowNum(num)}");
