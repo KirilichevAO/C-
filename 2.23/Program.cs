9fa35a8f23c124123a512a993e50d67d0bf120ea
North <North@MacBook-Air-MavBook.local>
2024-01-17 14:23:51 +0700

    2.23

---
+++ 2.23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Square (int num)
+{
+    for (int i = 1; i <= num; i++)
+    {   
+        Console.WriteLine($"{i} \t {i * i * i}");
+    }
+}
+
+if (number < 1)
+{
+    Console.Write("Введено некорректное значение!");
+    return;
+}
+
+Square(number);
