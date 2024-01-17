4c85ca2d8d1e74b81c47839984f272b449d70706
North <North@MacBook-Air-MavBook.local>
2024-01-17 14:22:58 +0700

    2.19

---
+++ 2.19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Palindrome (int num)
+{
+    if (num / 10000 == num % 10 && num / 1000 % 10 == num % 100 / 10)
+    {
+        Console.Write($"{num} -> да");
+    }
+    else Console.Write($"{num} -> нет");
+}
+
+if (number < 10000 || number > 99999)
+{
+    Console.Write("Введено некорректное значение!");
+    return;
+}
+
+Palindrome(number);
