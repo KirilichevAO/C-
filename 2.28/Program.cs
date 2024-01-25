8fe04ea26b82fe43c518a5d59029c57c61e0a598
North <North@MacBook-Air-MavBook.local>
2024-01-25 15:05:39 +0700

    2.28

---
+++ 2.28/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int CompositionNumbers (int num)
+{
+    int composition = 1;
+
+    for (int i = 1; i <= num; i++)
+    {
+        composition = composition * i;
+    }
+    return composition;
+}
+
+int result = CompositionNumbers(number);
+Console.WriteLine($"{number} -> {result}");
