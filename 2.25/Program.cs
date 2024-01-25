971f7e2015b44e62ea86d439cb7628440641e16a
North <North@MacBook-Air-MavBook.local>
2024-01-25 15:05:11 +0700

    2.25

---
+++ 2.25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int NaturalNum (int num1, int num2)
+{
+    int res = num1;
+    for (int i = 1; i < num2; i++)
+    {
+        res = res * num1;
+    }
+    return res;
+}
+
+int result = NaturalNum(a, b);
+Console.WriteLine(result);
