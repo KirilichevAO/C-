d4365760677adead11bcaa077b06533ba9665420
North <North@MacBook-Air-MavBook.local>
2024-01-25 15:05:55 +0700

    2.30

---
+++ 2.30/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+Console.Clear();
+Console.Write("Введите длинну массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[length];
+
+void RandomArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+}
+
+void PrintArray (int[] ar)
+{
+    for (int j = 0; j < ar.Length; j++)
+    {
+        Console.Write($"{ar[j]} ");
+    }
+}
+
+RandomArray(array);
+PrintArray(array);
