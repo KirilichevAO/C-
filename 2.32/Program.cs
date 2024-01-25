87eba4de1a395e1f80d9bc257b1c80108da7d4e6
North <North@MacBook-Air-MavBook.local>
2024-01-25 15:06:09 +0700

    2.32

---
+++ 2.32/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу замена элементов массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArrayAndRandom (int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] ArrayReversPozitivNegativ (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = array[i] * (-1);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] arr = CreateArrayAndRandom(7, -9, 15);
+PrintArray(arr);
+Console.WriteLine();
+int[] arrResult = ArrayReversPozitivNegativ(arr);
+PrintArray(arrResult);
