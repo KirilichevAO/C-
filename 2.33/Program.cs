d84b309ab10320548a4bc1b6588e8d6a1881bab6
North <North@MacBook-Air-MavBook.local>
2024-01-25 15:06:19 +0700

    2.33

---
+++ 2.33/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateRandomArray (int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray (int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+bool SearchNumberInArray (int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == num) return true;
+    }
+    return false;
+}
+
+Console.Write("Введите искомое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateRandomArray(9, 0, 10);
+PrintArray(arr);
+string res = SearchNumberInArray(arr, number) ? "да" : "нет";
+Console.WriteLine($" -> {res}");
