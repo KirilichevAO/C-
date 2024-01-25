2938553725b66c7ded26fcfc1751efe1902d49c0
North <North@MacBook-Air-MavBook.local>
2024-01-25 15:05:00 +0700

    2.24

---
+++ 2.24/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Summa (int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i; // sum = sum + i;
+    }
+    return sum;
+}
+
+Console.Write($"{number} -> {Summa(number)}");
