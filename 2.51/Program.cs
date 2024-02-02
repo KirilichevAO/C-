8d0579d9ea12d7edd18581351a0f093d9d9a960d
North <North@MacBook-Air-MavBook.local>
2024-02-02 12:07:59 +0700

    2.51

---
+++ 2.51/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+int[,] CreateRandomMatrix(int lines, int columns, int min, int max)
+{
+    int[,] matrix = new int[lines, columns];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+int IvenIndexesInSquared(int[,] matrix)
+{
+    int summaIvenIndex = 0;
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i == j) summaIvenIndex += matrix[i, j];
+        }
+    }
+    return summaIvenIndex;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] resultMatrix = CreateRandomMatrix(5, 3, 0, 10);
+int result = IvenIndexesInSquared(resultMatrix);
+PrintMatrix(resultMatrix);
+Console.Write($"{result}");
