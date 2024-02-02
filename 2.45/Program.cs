7826858ef2e515e2c6012528a25c3945cf7a3291
North <North@MacBook-Air-MavBook.local>
2024-02-02 12:07:02 +0700

    2.45

---
+++ 2.45/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] arrayOld = { 2, 4, 6, 8, 10, 12, 14 };
+
+int[] CopyArray(int[] array)
+{
+    int[] arrayNow = new int[array.Length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayNow[i] = array[i];
+    }
+    return arrayNow;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int[] resultArray = CopyArray(arrayOld);
+
+PrintArray(resultArray);
