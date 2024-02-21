017b36310c4ed12d22541cba7c23337e1b13d7bf
North <North@MacBook-Air-MavBook.local>
2024-02-21 10:14:31 +0700

    2.55

---
+++ 2.55/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] CreateRandomMatrix(int sizeLine, int sizeColumn, int min, int max)
+{
+    int[,] matrix = new int[sizeLine, sizeColumn];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+int[,] Revers(int[,] matrix)
+{
+    int[,] result = new int[matrix.GetLength(0), matrix.GetLength(1)];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            result[j, i] = matrix[i, j];
+        }
+    }
+    return result;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк: ");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+if (line == column)
+{
+    int[,] randomMatrix = CreateRandomMatrix(line, column, 0, 10);
+    PrintMatrix(randomMatrix);
+    Console.WriteLine();
+    int[,] reversMatrix = Revers(randomMatrix);
+    PrintMatrix(reversMatrix); 
+}
+
+else Console.Write("Это сделать невозможно!");
