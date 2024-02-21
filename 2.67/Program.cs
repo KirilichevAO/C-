2573c7a6b0c9f0306af15bffc78278ee45fc0ddb
North <North@MacBook-Air-MavBook.local>
2024-02-21 10:16:23 +0700

    2.67

---
+++ 2.67/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12 45 -> 9
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumbers(int num)
+{
+    if (num < 10) return num;
+    return num % 10 + SumNumbers(num / 10);
+}
+
+int sum = SumNumbers(number);
+Console.Write(sum);
