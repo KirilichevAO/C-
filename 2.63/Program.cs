6dc36d6b166d474e1109f010f063ee041eafcac7
North <North@MacBook-Air-MavBook.local>
2024-02-21 10:15:43 +0700

    2.63

---
+++ 2.63/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Введите число: ");
+int nummber = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+
+NaturalNumbers(nummber);
